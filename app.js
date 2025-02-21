a5dc09710467c2746dc4d98894487a28e9715395
leeminho <twomean99@gmail.com>
2025-02-21 13:33:12 +0900

    .gitignore 변경

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import pkg from "@prisma/client"; // default export로 가져오기
 import cors from "cors";
+import multer from "multer";
+
 import { asyncHandler } from "./src/routes/asyncHandler.js";
 import productRouter from "./src/routes/products.js";
 import articleRouter from "./src/routes/article.js";
@@ -13,19 +15,29 @@ const {
   PrismaClientValidationError,
   PrismaClientKnownRequestError,
 } = pkg;
+const corsOption = {
+  origin: "http://localhost:3000",
+};
 
 const prisma = new PrismaClient();
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOption));
 app.use(express.json());
 
 app.use("/products", productRouter);
 app.use("/articles", articleRouter);
 app.use("/", productCommentRouter);
 app.use("/", articleCommentRouter);
+
+const upload = multer({ dest: "uploads/" });
+
+app.post("/files", upload.single("attachment"), (req, res) => {
+  console.log(req.file);
+  res.json({ message: "파일업로드" });
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`server Started on ${process.env.PORT}!!`);
